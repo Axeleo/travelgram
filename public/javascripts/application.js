09f9406179c959a9b7b30829199faed544f6c72e
Axel <dlaxel@hotmail.com>
2018-09-12 11:58:28 +1000

    attempting js fix for heroku

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,21 @@
 
-const drawer = document.querySelector(".drawer")
-const drawerOpenButton = document.querySelector(".drawer-open-button")
-const drawerCloseButton = document.querySelector(".drawer-close-button")
-const drawerToggle = document.querySelector(".drawer-toggle")
+// const drawer = document.querySelector(".drawer")
+// const drawerOpenButton = document.querySelector(".drawer-open-button")
+// const drawerCloseButton = document.querySelector(".drawer-close-button")
+// const drawerToggle = document.querySelector(".drawer-toggle")
 
-var drawerState = 'closed'
+// var drawerState = 'closed'
 
-drawerToggle.addEventListener('click', () => {
-  if (drawerState === 'closed') {
-    drawer.classList.add('drawer-open')
-    drawerOpenButton.classList.add('hidden')
-    drawerCloseButton.classList.remove('hidden')
-    drawerState = 'open'
-  } else {
-    drawer.classList.remove('drawer-open')
-    drawerOpenButton.classList.remove('hidden')
-    drawerCloseButton.classList.add('hidden')
-    drawerState = 'closed'
-  }
-})
+// drawerToggle.addEventListener('click', () => {
+//   if (drawerState === 'closed') {
+//     drawer.classList.add('drawer-open')
+//     drawerOpenButton.classList.add('hidden')
+//     drawerCloseButton.classList.remove('hidden')
+//     drawerState = 'open'
+//   } else {
+//     drawer.classList.remove('drawer-open')
+//     drawerOpenButton.classList.remove('hidden')
+//     drawerCloseButton.classList.add('hidden')
+//     drawerState = 'closed'
+//   }
+// })
