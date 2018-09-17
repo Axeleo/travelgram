90801e99924dfd525cd406c9336e4a9b0c7f2df3
Axel <dlaxel@hotmail.com>
2018-09-17 10:03:42 +1000

    fixed redirect uri

--- main.rb
+++ main.rb
@@ -129,7 +129,7 @@ get '/oauth/callback' do
      { 'client_id' => ENV['INSTAGRAM_CLIENT_ID'], 
       'client_secret' => ENV['INSTAGRAM_CLIENT_SECRET'],
       'grant_type' => 'authorization_code',
-      'redirect_uri' => 'http://localhost:4567/oauth/callback',
+      'redirect_uri' => 'https://travelgram1.herokuapp.com/oauth/callback',
       'code' => params[:code].to_s
       })
 
